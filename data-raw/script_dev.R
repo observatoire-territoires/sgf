f941d1a4497429108befd13124cbf2b99cb26123
matgarnier <mathieu.garnier@cget.gouv.fr>
2019-08-28 18:12:01 +0200

    modif description

--- data-raw/script_dev.R
+++ data-raw/script_dev.R
@@ -16,6 +16,8 @@ my_desc$set(Description = "Old French statistics")
 my_desc$set("URL","https://github.com/observatoire-territoires/sgf")
 my_desc$set("BugReports","https://github.com/observatoire-territoires/sgf/issues")
 
+#use_gpl3_license(name="mtmx")
+
 # save
 my_desc$write(file = "DESCRIPTION")
 
@@ -25,3 +27,7 @@ devtools::use_data_raw()
 
 # sauvegarde des dataframes dans dossier data
 usethis::use_data(geo_DEP_SGF_histo, indicateurs_SGF, data_SGF, overwrite = T)
+
+
+# vérification de l'intégrité du package
+devtools::check()
